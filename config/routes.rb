2bc84d639489a00896325976a6e83da08a7b3e01
IOLevi <337@holbertonschool.com>
2019-02-07 00:32:24 +0000

    finish static pages tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'foo/bar'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
+  get 'static_pages/about'
+  
+  root 'static_pages#home'
 
-  root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
